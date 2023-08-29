726238ca3ea39ee2c9d77ba1cb5a3dda0dea4c75
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-29 16:13:55 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by 1234 on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
