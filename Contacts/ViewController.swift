19de221169e50dcb3e1d1e3622f62b27086c9d21
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-29 17:03:07 +0300

    1. is initial 2. Added data source methods for table view

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -17,3 +17,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var configCell = cell.defaultContentConfiguration()
+        configCell.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configCell
+        return cell
+    }
+    
+}
+
