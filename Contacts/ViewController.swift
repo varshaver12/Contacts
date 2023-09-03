c7b2b536b35d947e6a4a515aa00a680c6734dffd
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-03 08:22:53 +0300

    1. Added work with reused cell

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -22,11 +22,22 @@ extension ViewController: UITableViewDataSource {
         return 50
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var cell: UITableViewCell
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "contactReuseCell") {
+            cell = reuseCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "contactReuseCell")
+        }
+        configureCell(cell: &cell, for: indexPath)
+        return cell
+    }
+    
+    private func configureCell(cell: inout UITableViewCell, for indexPath: IndexPath) {
         var configCell = cell.defaultContentConfiguration()
         configCell.text = "Строка \(indexPath.row)"
+        configCell.secondaryText = "Новый контакт"
+        configCell.secondaryTextProperties.color = .orange
         cell.contentConfiguration = configCell
-        return cell
     }
     
 }
